a01b266b5b84c683ec7b348c02def1e24d15b103
shiba <cristophermoreno92@gmail.com>
2019-10-16 18:31:08 -0500

    ok

--- code/lab4/lab44.php
+++ code/lab4/lab44.php
@@ -5,31 +5,26 @@
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-  <title>Document</title>
+  <title>arreglo numero par</title>
 </head>
 
 <body>
   <?php
+
   echo '<form method="POST" action="./lab44.php">';
   echo 'meter numero par:<br />';
   echo '<input type="number" name="n" /><br />';
-
-
-  $arr = array();
-
-  if ($_POST['n'] % 2 == 0) {
-    array_push($arr, $_POST['n']);
-  } else {
-    echo 'introduzca valor correcto<br>';
-  }
-
   echo '<input type="submit" value="ok" name="btn_ok" /><br>';
-  echo '<input type="submit" value="mostrar_arr" name="btn_mostrar_arr" /><br>';
-  echo '</form>';
+  echo '<input type="submit" value="mostrar arreglo" name="btn_mostrar_arr" /><br>';
 
-  if (isset($_POST['mostrar_arr'])) {
-    echo 'mostrar arreglo<br>';
+  $n = $_POST['n'];
+
+
+  if ($n % 2 == 0) {
+    echo 'meter ' . $n;
   }
+
+  echo '</form>';
   ?>
 </body>
 
