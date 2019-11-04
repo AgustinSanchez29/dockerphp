37572f871908bdeff3997d3ef1b0eeb12dbce8bd
rho99 <cristophermoreno92@gmail.com>
2019-11-04 18:33:03 -0500

    fix

--- code/lab6/lab61.php
+++ code/lab6/lab61.php
@@ -3,5 +3,16 @@ class cliente {
     var $nombre;
     var $numero;
     var $peliculas_alquiladas;
+ 
+    function __construct($nombre, $numero){
+        $this->nombre=$nombre;
+        $this->numero=$numero;
+        $this->peliculas_alquiladas=array();
+    }
+
+    function __destruct(){
+        echo "<br> destruido: " . this->nombre;
+    }
+
     
 }
