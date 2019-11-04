bc22dd1cee8f14cd3eea2ceac8234a961da25135
rho99 <cristophermoreno92@gmail.com>
2019-11-04 18:53:55 -0500

    fix

--- code/lab6/lab61.php
+++ code/lab6/lab61.php
@@ -1,18 +1,32 @@
 <?php
-class cliente {
-    var $nombre;
-    var $numero;
-    var $peliculas_alquiladas;
- 
-    function __construct($nombre, $numero){
-        $this->nombre=$nombre;
-        $this->numero=$numero;
-        $this->peliculas_alquiladas=array();
+class cliente
+{
+    public $nombre;
+    public $numero;
+
+    public $peliculas_alquiladas;
+
+    public function __construct($nombre, $numero)
+    {
+        $this->nombre = $nombre;
+        $this->numero = $numero;
+        $this->peliculas_alquiladas = array();
     }
 
-    function __destruct(){
-        echo "<br> destruido: " . this->nombre;
+    public function __destruct()
+    {
+        echo "<br> destruido: " . $this->nombre;
     }
 
-    
+    public function dame_numero()
+    {
+        return $this->numero;
+    }
 }
+
+//instanciamos objetos
+$cliente_1 = new cliente("pepe", 1);
+$cliente_2 = new cliente("roberto", 564);
+
+echo "<br> el identificador del cliente 1 es: " . $cliente_1->dame_numero;
+echo "<br> el identificador del cliente 2 es: " . $cliente_2->dame_numero;
