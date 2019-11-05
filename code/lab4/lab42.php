4415d6fd2a6bfc3ecbd3aaaaf5400e414d71fc94
rho99 <cristophermoreno92@gmail.com>
2019-11-05 01:04:34 -0500

    Remove duplicated directory

--- code/lab4/lab42.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$f = 1;
-for ($i = $_POST['n']; $i > 0; $i--) {
-    echo $i . ' <br>';
-    $f = $f * $i;
-}
-echo 'factorial: ' . $f;
--- code/lab6/lab62.php
+++
@@ -1,3 +0,0 @@
-<?php
-include("class_lib.php");
-//intanciamos un objeto de la clase de class-lib
