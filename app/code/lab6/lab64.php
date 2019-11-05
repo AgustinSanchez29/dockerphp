6121970fa63d4070e15a176c49b23bc60952389b
rho99 <cristophermoreno92@gmail.com>
2019-11-05 06:15:41 -0500

    lab6

--- app/code/lab6/lab64.php
+++ app/code/lab6/lab64.php
@@ -6,3 +6,6 @@ $foo = new Foo();
 print $foo->staticvalor() . "value (2)<br>";
 
 print $foo->$mystatic . " balue (3) <br>";
+print Bar::$mystatic . "value (4)<br>";
+$bar = new Bar();
+print $bar->foostatic() . "value(5)";
